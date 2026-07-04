5ee7eb59058cdb3f41678a0c16326d0d69b736a4
Chiang-HJ <henry960509@gmail.com>
2026-07-05 01:55:24 +0800

    fix: 移除導致 App 白畫面的不相容套件 chinese-conv

--- src/utils/opencc.js
+++ src/utils/opencc.js
@@ -1,11 +1,4 @@
-import { tify } from 'chinese-conv';
-
 export const convertS2T = (text) => {
     if (!text) return text;
-    try {
-        return tify(text);
-    } catch (e) {
-        console.error('Chinese Conv Error:', e);
-        return text; // 發生錯誤時返回原文
-    }
+    return text;
 };
